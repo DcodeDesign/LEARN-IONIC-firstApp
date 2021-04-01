2242e9ed2e8a2d2bfbd6725d5874a46a67a69b33
DcodeDesign <gravythomas@gmail.com>
2021-04-01 09:11:26 +0200

    Commit: chrono

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'chrono',
+    loadChildren: () => import('./components/chrono/chrono.module').then(m => m.ChronoPageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pipes/addZeroBeforeNumber/add-zero-before-number.pipe.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {AddZeroBeforeNumberPipe} from './add-zero-before-number.pipe';
+
+@NgModule({
+  declarations: [AddZeroBeforeNumberPipe],
+  imports: [],
+  exports: [AddZeroBeforeNumberPipe],
+})
+
+export class AddZeroBeforeNumberPipeModule {}
